951e26a3a0f38d799c2555901152b4732d3137ce
isra17 <isra017@gmail.com>
2015-12-03 19:32:55 -0500

    Solve day2

---
+++ script/day2.exs
@@ -0,0 +1,21 @@
+
+data = IO.stream(:stdio, :line)
+  |> Enum.map(fn l ->
+      Regex.run(~r/(.+)x(.+)x(.+)/, l) |> tl
+        |> Enum.map(&(elem Integer.parse(&1), 0))
+  end)
+
+paper_qty = data
+  |> Enum.map(fn [l,w,h] ->
+    sides = [l*w,w*h,h*l]
+    Enum.sum(sides) * 2 + Enum.min(sides)
+  end)
+  |> Enum.sum
+
+ribbon_qty = data
+  |> Enum.map(&Enum.sort/1)
+  |> Enum.map(fn [l,w,h] -> l*2 + w*2 + l*w*h end)
+  |> Enum.sum
+
+IO.puts("Paper needed: #{paper_qty}")
+IO.puts("Ribbon needed: #{ribbon_qty}")
--- solve_all.sh
+++ solve_all.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 elixir script/day1.exs < input/day1
+elixir script/day2.exs < input/day2
